efc3dac21d5d1b2d98be1d9c3a132e0de42d8690
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2025-09-09 08:09:10 +0700

    9925-setup middleware-setup error and not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 
+import ErrorMiddleware from "@middleware/error.middleware";
+import NotFoundMiddleware from "@middleware/not-found.middleware";
+
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(ErrorMiddleware);
+app.use(NotFoundMiddleware);
