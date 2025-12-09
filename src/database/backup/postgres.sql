b3cf4f82a8a0e23593d351530e06c729f05f96c7
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2025-12-09 14:04:41 +0700

    [091225]-[database]-[backup]-update postgres.sql

--- src/database/backup/postgres.sql
+++ src/database/backup/postgres.sql
@@ -1,10 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE developer_accounts (
-    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NULL,
+    last_name VARCHAR(100),
     email VARCHAR(150) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    avatar_path TEXT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    avatar_path TEXT,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
 );
